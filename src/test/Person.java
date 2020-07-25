b24aee9274b83150efb7c4b92c2ae8f8dbaf18f2
ZYZR19 <1803255529@qq.com>
2020-07-25 19:45:32 +0800

    code

---
+++ src/test/Person.java
@@ -0,0 +1,5 @@
+package test;
+
+public class Person {
+    //json  对象变成字符串 以key value的形式存储
+}
---
+++ src/util/DBUtils.java
@@ -0,0 +1,5 @@
+package util;
+
+public class DBUtils {
+    private static String url = "jdbc:mysql://127.0.0.1:3306/terrymusic?useSSL==false";
+}
