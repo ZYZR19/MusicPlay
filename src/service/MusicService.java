c0895ff175a07e5bf70270fd98fb18139cd4ecc7
ZYZR19 <1803255529@qq.com>
2020-07-29 20:09:11 +0800

    code

---
+++ src/service/MusicService.java
@@ -0,0 +1,7 @@
+package service;
+
+import java.util.List;
+
+/*public class MusicService {
+    public List<>
+}*/
---
+++ src/service/UserService.java
@@ -0,0 +1,16 @@
+package service;
+
+import dao.UserDao;
+import entity.User;
+
+//降低耦合度 例如dao层的删除方法中调用了两个方法
+// 可单独写出去在service层连接在一起 尽量将业务进行剥离
+public class UserService {
+    //要做好前期的准备
+       public User login(User loginUser) {
+           UserDao userDao = new UserDao();
+           User user = userDao.login(loginUser);
+           return user;
+       }
+
+}
