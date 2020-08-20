d0947ae270e84a7993923486441808819ca5e5bc
ZYZR19 <1803255529@qq.com>
2020-08-20 23:08:41 +0800

    code

---
+++ src/dao/UserDaoTest.java
@@ -0,0 +1,20 @@
+package dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDaoTest {
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void selectByName() {
+    }
+}
